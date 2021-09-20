2253783c173aa4a096c31d4313ed99fa583e2046
oleg stabrovsky <olegstabr@gmail.com>
2021-09-20 21:11:57 +0700

    Init project

---
+++ SoloSet/SoloSet/SoloSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoloSetApp.swift
+//  SoloSet
+//
+//  Created by Олег Стабровский on 20.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SoloSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
