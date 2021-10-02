bbf33dffc14968a98fabd495ece5566411673699
oleg stabrovsky <olegstabr@gmail.com>
2021-10-02 20:53:54 +0700

    Draw triangle without formules; Next step - add formules and

--- SoloSet/SoloSet/Models/Shapes/Triangle.swift
+++ SoloSet/SoloSet/Models/Shapes/Triangle.swift
@@ -21,10 +21,10 @@ struct Triangle: ShapeProtocol, Shape {
 	
 	func path(in rect: CGRect) -> Path {		
 		var p = Path()
-		p.move(to: CGPoint(x: rect.midX, y: rect.minY))
-		p.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-		p.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-		p.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+		p.move(to: CGPoint(x: rect.maxX / 2, y: rect.maxY / 6))			// start drawing point
+		p.addLine(to: CGPoint(x: 10, y: 5 * rect.maxY / 6))				// left bot
+		p.addLine(to: CGPoint(x: rect.maxX - 10, y: 5 * rect.maxY / 6))	// right bot
+		p.addLine(to: CGPoint(x: rect.maxX / 2, y: rect.maxY / 6))		// top
 		return p
 	}
 	
