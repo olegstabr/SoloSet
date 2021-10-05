550c68b8aeeb151cbbb25afd682ec2d1808f63a4
oleg stabrovsky <olegstabr@gmail.com>
2021-10-05 20:35:34 +0700

    Create triangle view

--- SoloSet/SoloSet/Models/Shapes/Triangle.swift
+++ SoloSet/SoloSet/Models/Shapes/Triangle.swift
@@ -7,11 +7,15 @@
 
 import SwiftUI
 
-struct Triangle: ShapeProtocol, Shape {
+struct Triangle: ShapeProtocol, View {
 	var shapeType: ShapeType
 	var shading: Shading
 	var color: Color
 	var shapeCount: Int
+	
+	let numberOfStrips: Int = 5
+	let lineWidth: CGFloat = 3
+	let borderLineWidth: CGFloat = 3
 	
 	init(shading: Shading, color: Color, shapeCount: Int) {
 		shapeType = .triangle
@@ -20,11 +24,35 @@ struct Triangle: ShapeProtocol, Shape {
 		self.shapeCount = shapeCount
 	}
 	
-	func path(in rect: CGRect) -> Path {
-		return drawTriangle(in: rect)
+	var body: some View {
+		VStack {
+			ForEach(0..<shapeCount, id:\.self) { index in
+				HStack(spacing: 0) {
+					ForEach(0..<numberOfStrips) { number in
+						Color.white
+						color.frame(width: lineWidth)
+						if number == numberOfStrips - 1 {
+							Color.white
+						}
+					}
+				}.mask(TriangleShape())
+				.overlay(TriangleShape().stroke(color, lineWidth: borderLineWidth))
+			}
+		}
 	}
 	
-	private func drawOneTriangle(path: inout Path, startPoint: CGPoint, offset: CGFloat) { 
+}
+
+struct TriangleShape: Shape {
+	func path(in rect: CGRect) -> Path {
+		let firstThirdMaxX = CGFloat(rect.maxX / 2)
+		let firstThirdMaxY = CGFloat(rect.maxY)
+		let startDrawPoint = CGPoint(x: firstThirdMaxX, y: firstThirdMaxY / 6)
+		return drawOneTriangle(startPoint: startDrawPoint)
+	}
+	
+	private func drawOneTriangle(startPoint: CGPoint, offset: CGFloat = 0) -> Path {
+		var path = Path()
 		let maxX = startPoint.x
 		let maxY = startPoint.y
 		let startPoint = CGPoint(x: startPoint.x, y: startPoint.y + offset)
@@ -33,20 +61,6 @@ struct Triangle: ShapeProtocol, Shape {
 		path.addLine(to: CGPoint(x: maxX / 4, y: 5 * maxY + offset))		// left bot
 		path.addLine(to: CGPoint(x: 7 * maxX / 4, y: 5 * maxY + offset))	// right bot
 		path.addLine(to: startPoint)										// top
-	}
-	
-	private func drawTriangle(in rect: CGRect) -> Path {
-		var p = Path()
-		var offset = CGFloat(0)
-		let firstThirdMaxX = CGFloat(rect.maxX / 2)
-		let firstThirdMaxY = CGFloat(rect.maxY / CGFloat(shapeCount))
-		let startDrawPoint = CGPoint(x: firstThirdMaxX, y: firstThirdMaxY / 6)
-		
-		for _ in 0..<shapeCount {
-			drawOneTriangle(path: &p, startPoint: startDrawPoint, offset: offset)
-			offset += rect.maxY / CGFloat(shapeCount)
-		}
-		
-		return p
+		return path
 	}
 }
